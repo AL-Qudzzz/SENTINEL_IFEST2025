d984326e542ff363e76f2854731abea4aa1ca915
Muhammad Faiqul U.Dz. <faiqul84@gmail.com>
2025-09-26 02:39:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4200972128-d5334",
+  "appId": "1:247863180443:web:680ebd362705a885241a3e",
+  "apiKey": "AIzaSyDR0Pi6GVCQWCJDuq5MRi-3eiIXn0h85No",
+  "authDomain": "studio-4200972128-d5334.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "247863180443"
+};
