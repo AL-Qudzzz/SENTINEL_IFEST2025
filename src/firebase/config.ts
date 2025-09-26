c2e4be6b25417e66e4a0c44ba48c3f2a420bc930
Muhammad Faiqul U.Dz. <faiqul84@gmail.com>
2025-09-26 02:48:36 +0000

    letakan di .env dan gunakan API tersebut untuk mengakses Auth dan firest

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCJ0PP_rRwUB7Y-9MEXWi4KzMrB7uroNsY",
-  authDomain: "sentinel-26d8f.firebaseapp.com",
-  projectId: "sentinel-26d8f",
-  storageBucket: "sentinel-26d8f.appspot.com",
-  messagingSenderId: "379608883921",
-  appId: "1:379608883921:web:5426aa9fc45edb61ee9bf0",
-  measurementId: "G-S6FVTWRJQR"
-};
