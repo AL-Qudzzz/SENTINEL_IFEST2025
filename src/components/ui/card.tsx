da82e265f04b7c81e9e1060b42208b07b8b539da
Muhammad Zaky Ramadhan <zakyr9278@gmail.com>
2025-09-26 08:06:01 +0000

    ubah lah effect shadow menjadi drop shadow dengan X=7 Y=10 blur=8

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-md",
+      "rounded-lg border bg-card text-card-foreground filter drop-shadow-[7px_10px_8px_rgba(0,0,0,0.07)]",
       className
     )}
     {...props}
